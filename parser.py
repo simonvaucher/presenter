88967dc27465bbe0d847a46b69ab54a3ee2d52c8
simon.vaucher@gmail.com <simon.vaucher@gmail.com>
2013-12-02 16:42:54 +0200

    basic working verse parser

--- parser.py
+++ parser.py
@@ -4,9 +4,19 @@
 import sys
 import sqlite3 as lite
 from bs4 import BeautifulSoup
+from bs4 import NavigableString
+
+class Verse():
+    book = 0
+    chapter = 0
+    num = 0
+    translation = 0
+    text = ''
+    alt_text = ''
+    is_paragraph = False
 
 def doSomething():
-    f = open("OT/Bereshit/Bereshit01.htm","r")
+    f = open("OT/Bereshit/Bereshit02.htm","r")
     html_doc = f.read()
 
     soup = BeautifulSoup(html_doc)
@@ -15,12 +25,71 @@ def doSomething():
     print('all good inside parser')
     return soup.find_all(["p"])[0].get_text()
 
-print 'hello'
+def parse(url):
+    f = open(url,"r")
+    html_doc = f.read()
 
-con = lite.connect('scriptures.db')
-with con:
-    cur = con.cursor()    
-    # cur.execute("DROP TABLE Books")
-    # cur.execute("CREATE TABLE Books(Id INTEGER PRIMARY KEY, Name VARCHAR, Hebname VARCHAR)")
-    bla = doSomething()
-    cur.execute("INSERT INTO Books(Name, Hebname) VALUES('Bereshit', ?)", (bla,))
+    soup = BeautifulSoup(html_doc)
+    p = soup.p
+    verses = []
+    verse = Verse()
+    num = 0
+    record = False
+    record_next = False
+    is_paragraph_next = True
+    close_tag = ''
+
+    for e in p.next_elements:
+        if not isinstance(e, NavigableString):
+            if e.name == 'b':
+                if 'class' in e.attrs:
+                    num += 1
+                    verse.num = num
+                    if is_paragraph_next:
+                        verse.is_paragraph = True
+                        is_paragraph_next = False
+                    record_next = True
+                continue # this is for the html-1 b tag, ARGH
+
+
+            if e.name in ['big', 'small']:
+                verse.text += '<' + e.name + '>'
+                close_tag = '</' + e.name + '>'
+                continue
+
+            if e.get('name'):
+                record = False
+                verses.append(verse)
+                verse = Verse()
+                continue
+
+            if e.name == 'p':
+                is_paragraph_next = True
+                continue
+
+            if e.name == 'a' and 'href' in e.attrs:
+                record = False
+
+
+        if record_next:
+            record_next = False
+            record = True
+            continue
+
+        if record:
+            verse.text += unicode(e)
+            if close_tag:
+                verse.text += close_tag
+                close_tag = ''
+
+    verses.append(verse) # don't forget to append last verse...
+    return verses
+
+
+# con = lite.connect('scriptures.db')
+# with con:
+#     cur = con.cursor()
+#     # cur.execute("DROP TABLE Books")
+#     # cur.execute("CREATE TABLE Books(Id INTEGER PRIMARY KEY, Name VARCHAR, Hebname VARCHAR)")
+#     bla = doSomething()
+#     cur.execute("INSERT INTO Books(Name, Hebname) VALUES('Bereshit', ?)", (bla,))
--- presenter.py
+++ presenter.py
@@ -4,8 +4,8 @@
 import sqlite3 as lite
 import sys
 from PyQt4.QtGui import *
+from PyQt4.QtCore import *
 import parser
-
 
 class Presenter(QWidget):
     def __init__(self, text):
@@ -13,40 +13,49 @@ class Presenter(QWidget):
         self.initUI(text)
     
     def initUI(self, text):
-        txt = ""
-        con = lite.connect('scriptures.db')
+        # txt = ""
+        # con = lite.connect('scriptures.db')
 
-        with con:
-            cur = con.cursor()    
-            cur.execute("SELECT Name, Hebname FROM Books WHERE Name=:name", {"name": "Bereshit"})
+        # with con:
+        #     cur = con.cursor()    
+        #     cur.execute("SELECT Name, Hebname FROM Books WHERE Name=:name", {"name": "Bereshit"})
 
-            rows = cur.fetchall()
-            txt = rows[-1]
-            print txt
+        #     rows = cur.fetchall()
+        #     txt = rows[-1]
+        #     print txt
 
         self.resize(550, 550)
         self.move(400, 200)
-        self.setWindowTitle('test')
+        self.setWindowTitle('presenter / parser test')
 
-        txtEdit = QTextEdit(self)
-        txtEdit.setText(txt[1])
+        txtEdit = QTextEdit()
+        txtEdit.setText(text)
 
+        hbox = QHBoxLayout()
+        # hbox.addStretch(0)
+        hbox.addWidget(txtEdit)
+        self.setLayout(hbox)   
         self.show()
 
+    def keyPressEvent(self, e):
+        if e.key() == Qt.Key_Escape:
+            self.close()
 def main():
-    txt = parser.doSomething()
-    # print txt
+    # txt = parser.doSomething()
+    # print txt[::-1]
+    url = "OT/Bereshit/Bereshit03.htm"
+
+    verses = parser.parse(url)
+    print verses[0].text
+    txt = ''
+    for v in verses:
+        line = u'<small>{}</small>{}'.format(v.num, v.text)
+        if v.is_paragraph:
+            line = "<p>" + line
+        txt += line
+    print txt
 
     app = QApplication(sys.argv)
-
-    # w = QWidget()
-    # w.resize(550, 550)
-    # w.move(400, 200)
-    # w.setWindowTitle('test')
-
-    # txtEdit = QTextEdit()
-    # txtEdit.setText('bla')
-    # w.show()
 
     presenter = Presenter(txt)
     sys.exit(app.exec_())
